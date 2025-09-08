5826efe07454a205ec6869fdcf8bdb41d4e1464c
Myrty <derusebastien@gmail.com>
2025-09-08 20:41:14 +0200

    chore: init solution architecture

--- Oni.AppHost/AppHost.cs
+++ Oni.AppHost/AppHost.cs
@@ -1,12 +1,8 @@
-var builder = DistributedApplication.CreateBuilder(args);
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-var applicationservice = builder.AddProject<Projects.Oni_Application>("applicationservice")
-    .WithHttpHealthCheck("/health");
 
 builder.AddProject<Projects.Oni_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(applicationservice)
-    .WaitFor(applicationservice);
+    .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
